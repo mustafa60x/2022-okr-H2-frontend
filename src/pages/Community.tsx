7572944f1c65646bf336bf1a0bc1f3741f9a86ab
Mustafa Turkoz <mturkoz93@gmail.com>
2022-12-23 16:59:04 +0300

    installed classnames

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,6 +1,6 @@
+import classNames from "classnames";
 import { useState } from "react";
 import Modal from "../components/UI/Modal";
-
 
 const Community = (props) => {
   const [modalIsShown, setModalIsShown] = useState(false);
@@ -17,19 +17,27 @@ const Community = (props) => {
     <div>
       <h1>Community</h1>
 
-      <button onClick={ () => showModalHandler() }>showModalHandler</button>
+      <button onClick={() => showModalHandler()}>showModalHandler</button>
 
-      { modalIsShown && <Modal onClose={hideModalHandler}>
-        <h3>My Test Modal</h3>
-        mustafa türköz
-      </Modal> }
+      {modalIsShown && (
+        <Modal onClose={hideModalHandler}>
+          <h3>My Test Modal</h3>
+          mustafa türköz
+        </Modal>
+      )}
 
       <div className="bg-red-500">
-        <h3 className="text-xl font-semibold text-gray-400">Tailwind Başlık</h3>
+        <button
+          className={classNames({
+            "py-4 px-3 m-3 text-white/90 bg-green-400 hover:bg-red-600": true,
+            "bg-yellow-400": modalIsShown === true,
+          })}
+        >
+          Button
+        </button>
 
-        <button className="text-white/50 bg-green-400 hover:bg-red-600">Button</button>
+        <h3 className="text-xl font-semibold text-gray-100">Tailwind Başlık</h3>
       </div>
-      
     </div>
   );
 };
