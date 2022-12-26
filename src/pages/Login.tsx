7e04fe4d6015ed1eeabde7fbff6e7836ceb7ca36
Mustafa Turkoz <mturkoz93@gmail.com>
2022-12-26 23:46:10 +0300

    installed formik

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,24 +5,37 @@ import {AuthService} from "../services";
 
 import { Helmet } from "react-helmet"
 
+import { useFormik, Formik, Form, Field } from "formik";
+
 const Login = () => {
   const { user, dispatch } = useAuth() as any
 
   const [name, setName] = useState("mustafa");
 
   const login = async () => {
-
     const response = await AuthService.login({ username: 'ali', password: '123'}) as any
-    /* const newUser = {
-      name: 'Mustafa',
-      id: 1
-    } */
 
     dispatch({
       type: 'LOGIN',
       payload: response
     })
   }
+
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: async (values) => {
+      console.log(values)
+      const response = await AuthService.login({ username: values.username, password: values.password }) as any
+
+      dispatch({
+        type: 'LOGIN',
+        payload: response
+      })
+    } 
+  })
 
   return (
     <>
@@ -33,19 +46,61 @@ const Login = () => {
       <h1>Login Page</h1>
 
       <div>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />{" "}
-        <br />
-        {name}
-        <br />
+        <Formik initialValues={{
+          username: '',
+          password: '',
+          gender: 1,
+          tags: ['Seyahat', 'Kitap'],
+          avatar: ''
+        }} onSubmit={async (values) => {
+          console.log(values)
+          const response = await AuthService.login({ username: values.username, password: values.password }) as any
+
+          dispatch({
+            type: 'LOGIN',
+            payload: response
+          })
+        }}>
+          {({ values }) => (
+            <Form>
+              <Field name="username"></Field> <br />
+              <Field name="password" type="password"></Field> <br />
+              <Field component="select" name="gender">
+                <option value={1}>Erkek</option>
+                <option value={2}>Kadın</option>
+              </Field> <br />
+
+              <Field component="select" name="tags" multiple={true}>
+                <option value="Seyahat">Seyahat</option>
+                <option value="Kitap">Kitap</option>
+                <option value="Müzik">Müzik</option>
+                <option value="Spor">Spor</option>
+                <option value="Resim">Resim</option>
+              </Field> <br />
+
+              {/* <Field type="file" name="avatar"></Field> */}
+
+              <div className="mt-6">
+                <button className="p-3 bg-slate-600 text-white" type="submit">Giriş Yap</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+
+        {/* <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Kullanıcı adı</label>
+          <input type="text" id="username" value={values.username} onChange={handleChange} /> <br />
+
+          <label htmlFor="password">Parola</label>
+          <input type="password" id="password" value={values.password} onChange={handleChange} />
+
+          <div className="mt-6">
+            <button className="p-3 bg-slate-600 text-white" type="submit">Giriş Yap</button>
+          </div>
+        </form> */}
       </div>
 
-      <div className="mt-6">
-        <button className="p-3 bg-slate-600 text-white" onClick={login}>Giriş Yap</button>
-      </div>
+      
     </>
   );
 };
