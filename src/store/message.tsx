856eb958e774e36253d3d657611015c44a58bf4d
Mustafa Turkoz <mturkoz93@gmail.com>
2023-01-03 10:39:02 +0300

    updated message store

--- src/store/message.tsx
+++ src/store/message.tsx
@@ -1,12 +1,24 @@
 import create from "zustand"
 
 const useStore = create((set: any, get: any) => ({
+    GENERAL_CHAT_ID: "000111",
     selectedUser: {}, // default id for general chat ex: 000111
-    setSelectedUser: (userData) => set({ selectedUser: userData }),
+    setSelectedUser: (data) => set({ selectedUser: data }),
+
+    selectedRoom: {}, // default id for general chat ex: 000111
+    setSelectedRoom: (data) => set({ selectedRoom: data }),
+
     messages: [],
-    addMessage: (message) => set(state => ({ messages: [...state.messages, message] })),
+    addMessage: (message) => set(state => {
+        if(state.selectedRoom?._id === message.roomId) {
+            return { messages: [...state.messages, message] }
+        }
+
+        return { messages: state.messages }
+    }),
     setAllMessages: (messages) => set({ messages: messages }),
     destroyAllMessages: () => set({ messages: [] }),
+
     users: [],
     addUser: (user) => set(state => {
         const foundedUser = state.users.find(item => item._id === user._id)
@@ -18,7 +30,23 @@ const useStore = create((set: any, get: any) => ({
             }
         }
     }),
+    setAllUsers: (users) => set({ users: users }),
     destroyAllUsers: () => set({ users: [] }),
+
+    // room
+    rooms: [],
+    addRoom: (room) => set(state => {
+        const founded = state.rooms.find(item => item._id === room._id)
+        if (!!founded) {
+            return state.rooms
+        } else {
+            return {
+                rooms: [...state.rooms, room]
+            }
+        }
+    }),
+    setAllRooms: (rooms) => set({ rooms: rooms }),
+    destroyAllRooms: () => set({ rooms: [] }),
 }))
 
 export default useStore;
